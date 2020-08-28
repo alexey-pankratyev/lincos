44408653b5c5dc68234a05c2add2031797af0d56
Alexey Pankratyev <a_pankratyev@wargaming.net>
2020-08-28 18:02:59 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# lincos
- The goal of this project is to simplify the deployment of applications to Kubernetes without using secondary tools like helm, kustomize. For example helm will be integrated with https://pkg.go.dev/helm.sh/helm/v3
+# Lincos
+___
+ The goal of this project is to simplify the deployment of applications to Kubernetes without using secondary tools like helm, kustomize. For example helm will be integrated with https://pkg.go.dev/helm.sh/helm/v3_
+ ___
+
+example: 
+```
+
+```
---
+++ src/cmd/helm.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	//"fmt"
+
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/cli"
+)
+
+var (
+	helmCmd = &cobra.Command{
+		Use:   "helm",
+		Short: "Run helm commands",
+		Run:   runHelm,
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(helmCmd)
+
+}
+
+func runHelm(cmd *cobra.Command, args []string) {
+
+	settings := cli.New()
+
+	actionConfig := new(action.Configuration)
+	// You can pass an empty string instead of settings.Namespace() to list
+	// all namespaces
+	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+		log.Printf("%+v", err)
+		os.Exit(1)
+	}
+
+	client := action.NewList(actionConfig)
+	// Only list deployed
+	client.Deployed = true
+	results, err := client.Run()
+	if err != nil {
+		log.Printf("%+v", err)
+		os.Exit(1)
+	}
+
+	for _, rel := range results {
+		log.Printf("%+v", rel)
+	}
+
+}
